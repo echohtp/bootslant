40522114a5a6e076a8f0bb2f224a8329666065f4
Jason Schorr <echohtp@gmail.com>
2015-01-22 19:53:27 -0800

    inital commit

---
+++ server.js
@@ -0,0 +1,68 @@
+var Hapi = require('hapi');
+
+var sOptions = {
+    host: 'localhost',
+    port: 8950
+};
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection(sOptions);
+
+server.route({
+    method: 'GET',
+    path: '/img/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            path: './img'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/js/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            path: './js'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/fonts/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            path: './fonts'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/css/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            path: './css'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, reply){
+        reply.file('index.html');
+    }
+});
+
+
+// Start the server
+server.start(function() {
+    console.log('server started: http://' + sOptions.host + ':' + sOptions.port);
+});
